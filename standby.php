74a67e3f2c30172e746e5de027f667469558f927
Poojan Mehta <poojan.mehta2005@gmail.com>
2024-06-18 18:54:39 +0530

    License added

--- standby.php
+++ standby.php
@@ -1,10 +1,31 @@
 <?php
+/**
+ * @package SiteStandby
+ */
 /*
 Plugin Name: Site Standby
 Description: Easily enable a customizable maintenance mode page for your WordPress site. Keep visitors informed with personalized messages and designs while you update your website.
 Version: 1.0.0
 Author: PJM
+License: GPLv2 or later
 */
+
+/*
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+    Copyright (C) 2024 Poojan Mehta, "PJM"
+ */
 
 if (!defined('ABSPATH')) {
     exit; // Exit if accessed directly
